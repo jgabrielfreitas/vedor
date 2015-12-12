dbf81fc4c5e3b7abecaf0c8303c79a5a92b377bc
JGabrielFreitas <jgabrielafreitas@gmail.com>
2015-12-12 02:43:09 -0200

    parse fix and new splash

--- app/src/main/java/hackpuc/vedor/SplashActivity.java
+++ app/src/main/java/hackpuc/vedor/SplashActivity.java
@@ -21,23 +21,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Parse.initialize(this);
+        initialize();
 
         // Waiting 2 seconds before start main project
-//        Thread timer = new Thread(){
-//            public void run(){
-//                try {
-//                    sleep(2000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                } finally {
-//
-//                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-//                    finish();
-//                }
-//            }
-//        };
-//        timer.start();
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 
     protected void onResume() {
@@ -59,6 +59,12 @@ public class SplashActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initialize() {
+        try{
+            Parse.initialize(this);
+        }catch (Exception e) {}
     }
 
 }
