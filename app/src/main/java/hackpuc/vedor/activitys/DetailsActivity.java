07a6fe253d9d7b5f92fc3087441c189ed101f4ac
cristiandanner <cristiandanner@gmail.com>
2015-12-12 19:43:40 -0200

    Search View

---
+++ app/src/main/java/hackpuc/vedor/activitys/DetailsActivity.java
@@ -0,0 +1,15 @@
+package hackpuc.vedor.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hackpuc.vedor.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
