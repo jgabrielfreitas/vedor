88d49fb5b53176dbb68b5949399770e7ac5983e9
JGabrielFreitas <jgabrielafreitas@gmail.com>
2015-12-12 11:36:52 -0200

    HOT FIX

--- app/src/main/java/hackpuc/vedor/adapter/CandidateAdapter.java
+++ app/src/main/java/hackpuc/vedor/adapter/CandidateAdapter.java
@@ -28,37 +28,33 @@ public class CandidateAdapter extends BaseAdapter {
         this.politicList = politicList;
     }
 
-    @Override
     public int getCount() {
         return politicList.size();
     }
 
-    @Override
     public Politic getItem(int position) {
         return politicList.get(position);
     }
 
-    @Override
     public long getItemId(int position) {
         return position;
     }
 
-    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Politic politic = politicList.get(position);
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.expandable_item, parent, false);
 
-        ImageView partyImageView   = (ImageView) convertView.findViewById(R.id.partyImageView);
-        TextView nameTextView     = (TextView) convertView.findViewById(R.id.nameTextView);
+        ImageView partyImageView  = (ImageView) view.findViewById(R.id.partyImageView);
+        TextView nameTextView     = (TextView) view.findViewById(R.id.nameTextView);
 
-        TextView partyTextView    = (TextView) convertView.findViewById(R.id.partyTextView);
-        CheckBox favoriteCheckBox = (CheckBox) convertView.findViewById(R.id.favoriteCheckBox);
+        TextView partyTextView    = (TextView) view.findViewById(R.id.partyTextView);
+        CheckBox favoriteCheckBox = (CheckBox) view.findViewById(R.id.favoriteCheckBox);
 
-        TextView statusTextView   = (TextView) convertView.findViewById(R.id.statusTextView);
+        TextView statusTextView   = (TextView) view.findViewById(R.id.statusTextView);
 
 
-        partyImageView.setImageResource(politic.getCandidatePartyInitials());
+//        partyImageView.setImageResource(politic.getCandidatePartyInitials());
 		nameTextView.setText(politic.getCandidateName());
 
 		partyTextView.setText(politic.getCandidatePartyNumber() + " - " + politic.getCandidatePartyName());
